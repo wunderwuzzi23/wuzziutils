a2d7c87186192e2377b044b08c5b96152fb00aea
wunderwuzzi23 <rehberger@hotmail.com>
2020-03-05 10:25:38 -0800

    Adding enabled feature

--- mailutil/mailutil.go
+++ mailutil/mailutil.go
@@ -14,13 +14,14 @@ type configuration struct {
 	smtpPassword string
 	mailFrom     string
 	mailTo       string
+	enabled      bool
 }
 
 //MailConfig holds the settings when sending email via mailutil
 var MailConfig configuration
 
 // SetConfiguration for sending email
-func SetConfiguration(smtpServer string, port int, account string, password string, mailFrom string, mailTo string) {
+func SetConfiguration(smtpServer string, port int, account string, password string, mailFrom string, mailTo string, enabled bool) {
 	config := configuration{}
 	config.smtpServer = smtpServer
 	config.smtpPort = port
@@ -28,13 +29,27 @@ func SetConfiguration(smtpServer string, port int, account string, password stri
 	config.smtpPassword = password
 	config.mailFrom = mailFrom
 	config.mailTo = mailTo
+	config.enabled = enabled
 	MailConfig = config
+}
+
+//SetEnabled allows to configure the ability to enable/disable sending email
+func SetEnabled(enabled bool) {
+	if &MailConfig == nil {
+		log.Panicln("Need to call SetConfiguration first.")
+	}
+
+	MailConfig.enabled = enabled
 }
 
 //SendMail provides a quick way to send a message to recipients
 func SendMail(subject string, message string) {
 	if &MailConfig == nil {
 		log.Panicln("Need to call SetConfiguration first.")
+	}
+
+	if MailConfig.enabled == false {
+		return
 	}
 
 	m := gomail.NewMessage()
